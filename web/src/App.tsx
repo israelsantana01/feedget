9d959a58d29ae58e994b8eb72b25af4ac6539364
israelsantana01 <israelofi2@gmail.com>
2022-05-02 09:42:07 -0300

    install tailwind css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,7 @@ interface ButtonProps {
 }
 
 function Button(props: ButtonProps) {
-
-  return <button>{ props.text ?? 'Default' }</button>;
+  return <button className="bg-violet-500 p-2">{ props.text ?? 'Default' }</button>;
 }
 
 function App() {
