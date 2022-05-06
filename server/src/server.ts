26ca5e096de6e2b56ef23465455c05fe3e22af2c
israelsantana01 <israelofi2@gmail.com>
2022-05-06 10:22:57 -0300

    setup feedbacks route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json());
 
-app.post('feedbacks', async (req, res) => {
+app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
 
   const feedback = await prisma.feedBack.create({
