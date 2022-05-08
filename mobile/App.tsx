5f0677ec61cac2a957a94ba64e6e5e1b43990e35
israelsantana01 <israelofi2@gmail.com>
2022-05-08 09:45:50 -0300

    use custom fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,8 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { theme } from './src/theme';
 import { Widget } from './src/components/widget';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <View 
       style={{
