0bf5416e26001e44fc59443dcdc8517f5df900ed
israelsantana01 <israelofi2@gmail.com>
2022-05-09 14:06:39 -0300

    integrating web app and the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,9 @@ import { routes } from './routes';
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+  limit: '50mb'
+}));
 app.use(routes);
 
 app.listen(3333, () => console.log('Server runnning...'));
--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../services/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -17,17 +19,26 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({
-      screenshot,
-      comment
-    });
-
-    onFeedbackSent();
+    setIsSendingFeedback(true);
+    
+    try {
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        comment,
+        screenshot
+      });
+      
+      setIsSendingFeedback(false);
+      onFeedbackSent();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -64,10 +75,10 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={!comment.length}
+            disabled={!comment.length || isSendingFeedback}
             className="p-2 w-full bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar Feedback
+            {isSendingFeedback ? <Loading /> : 'Enviar Feedback'}
           </button>
         </footer>
       </form>
