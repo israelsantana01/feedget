8974e21d3f89c410458d049008013690dc3a4fd0
israelsantana01 <israelofi2@gmail.com>
2022-05-09 15:15:17 -0300

    let server port definition optional

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,4 +9,4 @@ app.use(express.json({
 }));
 app.use(routes);
 
-app.listen(3333, () => console.log('Server runnning...'));
+app.listen(process.env.PORT || 3333, () => console.log('Server runnning...'));
